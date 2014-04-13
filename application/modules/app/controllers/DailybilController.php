377e4b331dddad09a64878c4d8a891a1b44b8892
Rémi Darricau <welcomebac@gmail.com>
2014-04-13 18:38:14 +0200

    DailyBil

--- application/modules/app/controllers/DailybilController.php
+++ application/modules/app/controllers/DailybilController.php
@@ -10,7 +10,18 @@ class DailybilController extends Lib_AC_AppController
 
 	public function indexAction()
 	{
+		if(isset($_POST['categories']))
+		{
+			$_POST['token'] = "abcd";
+			Lib_Dailybil_Api::create($_POST);
+		}
 		$this->view->categories = Lib_Dailybil_Api::categories();
-		$this->view->accounts = $this->mLink->findByAttributes(array('idUser'=>$this->session->user['id']));
+		$links = $this->mLink->findAllByAttributes(array('idUser'=>$this->session->user['id'],'type'=>Model_Link::TYPE_TWITTER));
+		foreach($links as $link)
+		{
+			$searches[$link['title']]= Lib_Dailybil_Api::searches("abcd");
+		}
+		$this->view->searches = $searches;
+		$this->view->links = $links;
 	}
 }
--- library/Lib/Dailybil/Api.php
+++ library/Lib/Dailybil/Api.php
@@ -1,7 +1,25 @@
 <?php
 class Lib_Dailybil_Api
 {
-	protected static $apiKey = "bYyv4qKwxwC10J7mYA";
+	public function create($post)
+	{
+		$url = 'http://dailybil.herokuapp.com/tweeter';
+		$fields = array(
+						'token' => $post['token'],
+						'categories'=> implode(",",$post['categories'])
+		);
+		$fields_string = "";
+		foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; } 
+		rtrim($fields_string,'&');
+		$ch = curl_init();
+		curl_setopt($ch,CURLOPT_URL,$url);
+		curl_setopt($ch,CURLOPT_POST,count($fields));
+		curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+		curl_exec($ch);
+		//close connection 
+		curl_close($ch);
+	}
 
 	public function categories()
 	{
@@ -10,10 +28,30 @@ class Lib_Dailybil_Api
 		curl_setopt($ch,CURLOPT_URL,$url);
 		curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 		if($categoriesStr = curl_exec($ch)) {
-			$categories = explode($categoriesStr,",");
+			$categories = explode(',',Zend_Json::decode($categoriesStr));
+		}
+		foreach($categories as $key => $categorie)
+		{
+			if($categorie == "")
+				unset($categories[$key]);
 		}
 		//close connection 
 		curl_close($ch);
 		return $categories;
+	}
+
+	public function searches($token)
+	{
+		$url = 'http://dailybil.herokuapp.com/tweeter';
+		$get = '?token='.$token;
+		$ch = curl_init();
+		curl_setopt($ch,CURLOPT_URL,$url.$get);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+		if($searchesStr = curl_exec($ch)) {
+			$searches = Zend_Json::decode($searchesStr);
+		}
+		//close connection 
+		curl_close($ch);
+		return $searches;
 	}
 }
