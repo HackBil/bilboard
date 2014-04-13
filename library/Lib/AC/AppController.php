ce8d39d9d1ebf1336850a27ac6200ee07dde312c
Rémi Darricau <welcomebac@gmail.com>
2014-04-13 15:27:20 +0200

    client = user

--- library/Lib/AC/AppController.php
+++ library/Lib/AC/AppController.php
@@ -16,7 +16,7 @@ class Lib_AC_AppController extends Zend_Controller_Action
 			if($this->config != null)
 				$this->session->setExpirationSeconds($this->config->duree_session*3600);
         }
-        if(!isset($this->session->client))
+        if(!isset($this->session->user))
             $this->_redirect('/connexion');
         $this->view->session = $this->session;
         
