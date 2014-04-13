45c63a92d561f71b9c38a064ae6894b964f66387
Rémi Darricau <welcomebac@gmail.com>
2014-04-13 16:52:02 +0200

    DailyBil & FlashMessage

--- library/Lib/AC/AppController.php
+++ library/Lib/AC/AppController.php
@@ -8,6 +8,14 @@ class Lib_AC_AppController extends Zend_Controller_Action
             $this->config = Zend_Registry::get('config');
         else
             $this->config = null;
+
+        $this->flashMessenger = $this->_helper->getHelper('FlashMessenger');
+        $messages = $this->flashMessenger->getMessages();
+        if (!empty($messages))
+        {
+            $this->view->flash   = $messages;
+            $this->flashMessenger->clearMessages();
+        }
                         
         // Gestion de la session
 		if(Zend_Registry::isRegistered('session'))
---
+++ library/Lib/Dailybil/Api.php
@@ -0,0 +1,9 @@
+<?php
+class Lib_Dailybil_Api
+{
+	protected static $apiKey = "bYyv4qKwxwC10J7mYA";
+
+	public function create($fichier,$filename,$folderDest,$type="pdf")
+	{
+	}
+}
